44b69dc9b385f76fbc9eba52c4ffc38a8cf630d6
clintgundersen <truegrael@yahoo.com>
2015-10-29 23:50:14 -0600

    finished logic and styled for mobile

--- lunarTrack.js
+++ lunarTrack.js
@@ -2,18 +2,55 @@ jQuery(document).ready(function() {
     var MONTH_LENGTH = 28;
     var MONTHS_IN_YEAR = 12;
 
-    var MOON_ONE_CYCLE = 14;
-    var MOON_TWO_CYCLE = 52;
+    var MOON_14_CYCLE = 14;
+    var MOON_52_CYCLE = 52;
 
-    var year;
-    var month;
-    var day;
+    var jYear =jQuery('#year');
+    var jMonth = jQuery('#month');
+    var jDay = jQuery('#day');
+
+    var year = parseInt(jYear.val() - 1);
+    var month = parseInt(jMonth.val() - 1);
+    var day = parseInt(jDay.val());
 
     var output = jQuery('#output');
 
     jQuery('#yesterday').click(decrementDay);
     jQuery('#tomorrow').click(incrementDay);
     jQuery('#getPhases').click(showMeTheCycle);
+
+    jDay.change(function(){
+        var input =  parseInt(jDay.val());
+        if (input <= MONTH_LENGTH && input > 0) {
+            day = input;
+            console.log("day updated to " + day);
+        } else {
+            jQuery('#day').val(day);
+            console.log("no update:" + day);
+        }
+    });
+
+    jMonth.change(function(){
+        var input =  parseInt(jMonth.val());
+        if (input <= MONTHS_IN_YEAR && input > 0) {
+            month = input - 1;
+            console.log("month updated to " + month);
+        } else {
+            jQuery('#month').val(month + 1);
+            console.log("no update:" + month);
+        }
+    });
+
+    jYear.change(function(){
+        var input = parseInt(jYear.val());
+        if (input > 0) {
+            year = input -1;
+            console.log("year updated to " + year + 1);
+        } else {
+            jQuery('#year').val(year + 1);
+            console.log("no update: " + month);
+        }
+    });
 
     //values represent the state of the moons on year 1, month 1, day 1
     var moonOneOffset = 0;
@@ -26,90 +63,146 @@ jQuery(document).ready(function() {
     var moonTwoCyclesToDate;
 
     function decrementDay() {
-        updateValues();
         console.log("decrementing");
-        jQuery('#day').val(parseInt(day - 1));
-        updateValues();
+        if ( day == 1) {
+            console.log("month" + month);
+            if (month == 0) {
+                //drop a year
+                console.log("boo, reverse new years");
+                if (year >= 0) {
+                    year--;
+                    month = MONTHS_IN_YEAR - 1;
+                    day = MONTH_LENGTH;
+                    jQuery('#month').val(month + 1);
+                    jQuery('#year').val(year + 1);
+                } else {
+                    console.log ("before the start of time. no change");
+                }
+            } else {
+                //bacu up a new month
+                month--;
+                refreshGui();
+                day = MONTH_LENGTH;
+            }
+        } else {
+            day--;
+        }
+        jQuery('#day').val(day);
         showMeTheCycle();
     }
 
     function incrementDay() {
-        updateValues();
         console.log("incrementing");
         if ( day == MONTH_LENGTH) {
-            if (month == MONTHS_IN_YEAR) {
+            console.log("month" + month);
+            if (month == MONTHS_IN_YEAR -1) {
                 //start a new year and a new month
                 console.log("happy new years");
                 year++;
-                month = 1;
+                month = 0;
                 day = 1;
+                jQuery('#month').val(month + 1);
+                jQuery('#year').val(year + 1);
             } else {
                 //start a new month
                 console.log("new month, from:" + month);
-                month = month + 2; //to offset the -1 to account for no year 0
+                month++;
                 console.log("to:" + month);
+                refreshGui();
+
                 day = 1;
             }
         } else {
             day++;
         }
         jQuery('#day').val(day);
-        updateValues();
         showMeTheCycle();
     }
 
-    function getLastNewMoonOne() {
-        var newMoonOne = moonOneCyclesToDate * MOON_ONE_CYCLE;
+    function getLastNewMoon14() {
+        var newMoonOne = moonOneCyclesToDate * MOON_14_CYCLE;
         return newMoonOne;
     }
 
-    function getLastNewMoonTwo() {
-        var newMoonTwo = moonTwoCyclesToDate * MOON_TWO_CYCLE;
+    function getLastNewMoon52() {
+        var newMoonTwo = moonTwoCyclesToDate * MOON_52_CYCLE;
         return newMoonTwo;
     }
 
-    function getCurrentCycleMoonOne () {
-        var currentMoonOne = totalDaysToDate - (getLastNewMoonOne() + moonOneOffset);
+    function getCurrentCycleMoon14 () {
+        var currentMoonOne = totalDaysToDate - (getLastNewMoon14() + moonOneOffset);
         return currentMoonOne;
     }
 
-    function getCurrentCycleMoonTwo () {
-        var currentMoonTwo = totalDaysToDate - (getLastNewMoonTwo() + moonTwoOffset);
+    function getCurrentCycleMoon52 () {
+        var currentMoonTwo = totalDaysToDate - (getLastNewMoon52() + moonTwoOffset);
         return currentMoonTwo;
     }
 
-    function updateValues() {
-        year = parseInt(jQuery('#year').val()) - 1;
-        month = parseInt(jQuery('#month').val()) - 1;
-        day =  parseInt(jQuery('#day').val());
+    function refreshGui() {
+        console.log("updating values");
+        jQuery('#year').val(year + 1);
+        jQuery('#month').val(month + 1);
+        jQuery('#day').val(day);
     }
 
     function showMeTheCycle () {
-        updateValues();
         console.log(year);
         console.log(month);
         console.log(day);
+        var MOON_14 = "Moraeithyl";
+        var MOON_52 = "Itiri'welun";
 
         var daysInYears = year * MONTHS_IN_YEAR * MONTH_LENGTH;
-        console.log ("days in the years:" + daysInYears);
         var daysInMonths = month * MONTH_LENGTH;
-        console.log ("days in the months:" + daysInMonths);
-        console.log ("days in the days:" + day);
 
         totalDaysToDate = daysInYears + daysInMonths + day;
-        console.log ("totalDays is: " + totalDaysToDate);
 
-        moonOneCyclesToDate = Math.floor(totalDaysToDate / MOON_ONE_CYCLE);
-        moonTwoCyclesToDate = Math.floor(totalDaysToDate / MOON_TWO_CYCLE);
+        moonOneCyclesToDate = Math.floor(totalDaysToDate / MOON_14_CYCLE);
+        moonTwoCyclesToDate = Math.floor(totalDaysToDate / MOON_52_CYCLE);
 
-        console.log ("moon One Cycles:" + moonOneCyclesToDate);
-        console.log ("moon Two Cycles:" + moonTwoCyclesToDate);
+        var moon14Current = getCurrentCycleMoon14() + 1;
+        var moon52Current = getCurrentCycleMoon52() + 1;
 
-        console.log ("last new One:" + getLastNewMoonOne());
-        console.log ("last new Two:" + getLastNewMoonTwo());
+        var outputString = MOON_14 + " cycle day: " + moon14Current + ".<br>";
+        outputString += getPhase(moon14Current, MOON_14_CYCLE);
+        outputString += "<br><br>" + MOON_52 + " cycle day: " + moon52Current + ".<br>";
+        outputString += getPhase(moon52Current, MOON_52_CYCLE);
 
-        console.log("current cycle one:" + getCurrentCycleMoonOne());
-        console.log("current cycle two:" + getCurrentCycleMoonTwo());
+        if (year == 1935 && month == 1 && day == 6) {
+            outputString += "<br><br><br> The snow fell in sheets, running down my neck and soaking me clean through.  I was filled with doubts as I stepped off the curb,  A hover messenger nearly knocking me to the ground for my troubles.  He shook his fist and cursed as he zipped down Electric Square.  I pulled my collar tighter, it is an easy thing for a man to say he needs you to stop a war, it is another thing to do it.";
+        }
+
+        jQuery('#output').html(outputString);
+    }
+
+    function getPhase(current, cycleLength) {
+        var eigth = cycleLength / 8;
+        console.log("eigth for " + cycleLength + " :" + eigth);
+        if (current <= eigth) {
+            return "New Moon";
+        }
+        else if (current <= eigth) {
+            return "Waxing Crescent";
+        }
+        else if (current <= eigth * 3) {
+            return "Waxing Quarter";
+        }
+        else if (current <= eigth * 4) {
+            return "Waxing Gibbous";
+        }
+        else if (current <= eigth * 5) {
+            return "Full Moon";
+        }
+        else if (current <= eigth * 6) {
+            return "Waning Gibbous";
+        }
+        else if (current <=  eigth * 7) {
+            return "Waning Quarter";
+        }
+        else if (current <= eigth * 8) {
+            return "Waning Crescent";
+        }
     }
 });
 
